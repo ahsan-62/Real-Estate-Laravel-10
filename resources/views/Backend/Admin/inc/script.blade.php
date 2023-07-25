c0136f04b52c93525dcdff313d72366ebaef959f
Ahsan Al Bashar <ahsan.cse.101792@gmail.com>
2023-07-25 17:31:45 +0600

    admin Panel setup

---
+++ resources/views/Backend/Admin/inc/script.blade.php
@@ -0,0 +1,15 @@
+<!-- core:js -->
+<script src="{{ asset('assets/admin') }}/vendors/core/core.js"></script>
+<!-- Plugin js page -->
+<script src="{{ asset('assets/admin') }}/vendors/flatpickr/flatpickr.min.js"></script>
+<script src="{{ asset('assets/admin') }}/vendors/apexcharts/apexcharts.min.js"></script>
+<!-- End plug{{ asset('assets/admin') }} this page -->
+
+
+<script src="{{ asset('assets/admin') }}/vendors/feather-icons/feather.min.js"></script>
+<script src="{{ asset('assets/admin') }}/js/template.js"></script>
+<!-- endinjec{{ asset('assets/admin') }}
+
+<!-- Custom js page -->
+<script src="{{ asset('assets/admin') }}/js/dashboard-dark.js"></script>
+<!-- End custom js for this page -->
